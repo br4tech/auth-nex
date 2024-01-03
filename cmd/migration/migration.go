4764662d3592f5493775400bf58cbb597b340c74
Guilherme Silva <guilherme.tr.silva@gmail.com>
2024-01-03 08:19:25 -0300

    Add setup migrate

---
+++ cmd/migration/migration.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/br4tech/auth-nex/config"
+	"github.com/br4tech/auth-nex/internal/adapter"
+	"github.com/br4tech/auth-nex/internal/core/port"
+	"github.com/br4tech/auth-nex/internal/model"
+)
+
+func main() {
+	cfg := config.GetConfig()
+	db := adapter.NewPostgresDatabase(&cfg)
+
+	AuthnexMigrate(db)
+}
+
+func AuthnexMigrate(db port.IDatabase) {
+	db.GetDb().Migrator().CreateTable(
+		&model.Tenant{},
+		&model.User{},
+		&model.Role{},
+	)
+}
