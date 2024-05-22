b4022eff064ad2704127f30a982d90a89fa1240e
Guilherme Silva <guilherme.tr.silva@gmail.com>
2024-05-22 08:40:45 -0300

    refactor in wire DI

--- cmd/wire.go
+++ cmd/wire.go
@@ -4,48 +4,38 @@
 package main
 
 import (
-  "github.com/br4tech/auth-nex/internal/core/usecase/auth"
+	"github.com/br4tech/auth-nex/internal/core/port"
+	"github.com/br4tech/auth-nex/internal/core/usecase/auth"
+	"github.com/br4tech/auth-nex/internal/core/usecase/company"
+	"github.com/br4tech/auth-nex/internal/core/usecase/tenant"
 	"github.com/br4tech/auth-nex/internal/handler"
 	"github.com/br4tech/auth-nex/internal/repository"
 	"github.com/google/wire"
 	"gorm.io/gorm"
 )
 
-
-//go:build wireinject
-// +build wireinject
-
-package main
-
-import (
-	"fmt"
-
-	"github.com/br4tech/go-with-gemini/config"
-	"github.com/br4tech/go-with-gemini/internal/adapter"
-	"github.com/br4tech/go-with-gemini/internal/core/port"
-	"github.com/br4tech/go-with-gemini/internal/core/usecase"
-	"github.com/br4tech/go-with-gemini/internal/handler"
-	"github.com/br4tech/go-with-gemini/internal/repository"
-	"github.com/google/wire"
-	"github.com/spf13/viper"
-	"gorm.io/gorm"
-)
-
-func InitializeUserHandler(db *gorm.DB) *handler.UserHandler {
+func InitializeUserHandler(db *gorm.DB) port.IUserHandler {
 	wire.Build(
 		repository.NewUserRepository,
 		auth.NewAuthUseCase,
 		handler.NewUserHandler,
+		wire.Bind(new(port.IUserHandler), new(*handler.UserHandler)),
 	)
 
 	return &handler.UserHandler{}
 }
 
-func InitializeTenantHandler(db *gorm.DB) *handler.TenantHandler {
+func InitializeTenantHandler(db *gorm.DB) port.ITenantHandler {
 	wire.Build(
 		repository.NewTenantRepository,
+		repository.NewCompanyRepository,
+		repository.NewUserRepository,
+		company.NewCompanyUseCase,
+		auth.NewAuthUseCase,
 		tenant.NewTenantUseCase,
 		handler.NewTenantHandler,
+
+		wire.Bind(new(port.ITenantHandler), new(*handler.TenantHandler)),
 	)
 
 	return &handler.TenantHandler{}
