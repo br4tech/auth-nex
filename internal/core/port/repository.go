24059b93b525191f19b5547f1bbeaf627f3a4753
Guilherme Silva <guilherme.tr.silva@gmail.com>
2024-08-17 15:34:34 -0300

    rwfactor: update at interfaces

--- internal/core/port/repository.go
+++ internal/core/port/repository.go
@@ -4,28 +4,28 @@ import "github.com/br4tech/auth-nex/internal/core/domain"
 
 type (
 	ICompanyRepository interface {
-		Create(company *domain.Company) error
+		Create(company *domain.Company) (*domain.Company, error)
 		FindById(id int) (*domain.Company, error)
 		Update(company *domain.Company) error
 		Delete(id int) error
 	}
 
 	IPermissionRepository interface {
-		Create(permission *domain.Permission) error
+		Create(permission *domain.Permission) (*domain.Permission, error)
 		FindById(id int) (*domain.Permission, error)
 		Update(permission *domain.Permission) error
 		Delete(id int) error
 	}
 
 	IProfileRepository interface {
-		Create(profile *domain.Profile) error
+		Create(profile *domain.Profile) (*domain.Profile, error)
 		FindById(id int) (*domain.Profile, error)
 		Upate(profile *domain.Profile) error
 		Delete(id int) error
 	}
 
 	ITenantRepository interface {
-		Create(tenant *domain.Tenant) error
+		Create(tenant *domain.Tenant) (*domain.Tenant, error)
 		FindById(id int) (*domain.Tenant, error)
 		FindByName(name string) (*domain.Tenant, error)
 		Update(tenant *domain.Tenant) error
@@ -33,7 +33,7 @@ type (
 	}
 
 	IUserRepository interface {
-		Create(user *domain.User) error
+		Create(user *domain.User) (*domain.User, error)
 		FindById(id int) (*domain.User, error)
 		Update(user *domain.User) error
 		Delete(id int) error
